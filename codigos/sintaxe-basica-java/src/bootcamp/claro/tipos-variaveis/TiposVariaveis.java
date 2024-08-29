82075c280db8f1863dcdedf95186e22635eeaa26
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-08-29 11:26:09 -0300

    add classe operadores

--- codigos/sintaxe-basica-java/src/bootcamp/claro/tipos-variaveis/TiposVariaveis.java
+++ codigos/sintaxe-basica-java/src/bootcamp/claro/tipos-variaveis/TiposVariaveis.java
@@ -1,6 +1,24 @@
 public class TiposVariaveis {
+
+    // Variavel: É uma área na memória, associada a um nome, qu epode armazenar valores de um determinado tipo.
+    // Constantes: São valores armazenados em memória que não podem ser modificados depois de declarados. São representados pela palavra reservada (final) seguido do tipo
+    //              Por convensão a constante sempre são escritas em caixa alta
+
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
-        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; // Conversão implicita (widening) de tipo sem perda de dados na atribuição
+        short numeroCurto2 = (short) numeroNormal;// Conversão explicita (narrowing), estou forçando a conversão de int para short
+
+        // Variavel: Consigo o valor da variavel armazenado na memória
+        int numero = 5;
+        numero = 10;
+
+        // Constante exemplo
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 10.75; // erro:  cannot assign a value to final variable VALOR_DE_PI
+
+
     }
 }
--- codigos/sintaxe-basica-java/src/bootcamp/claro/tipos-variaveis/operadores.java
+++ codigos/sintaxe-basica-java/src/bootcamp/claro/tipos-variaveis/operadores.java
@@ -22,4 +22,76 @@ public class operadores {
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
     }
+
+    public static void operadorUnario (String [] args) {
+
+        int numero = 5; // iniciei a variavel com valor 5
+
+        numero = - numero; // desta forma, eu falo para o interpretador que o valor da variavel foi alterado em todo o código
+
+        // System.out.println(- numero); // passa a ser um valor negativo somente neste trecho
+
+        System.out.println(numero); // volta o valor positivo original da variavel
+
+        // numero = + numero; // desta forma não seria convertido para + 5, porque deste jeito o + é sobre operador aritmetico soma
+
+        numero = numero * -1; // 
+        System.out.println(numero);
+    }
+
+    public static void operadorUnarioIncremento (String [] args) {
+
+        int numero = 5;
+
+        //x repeticao
+        numero ++; // é a mesma coisa de numero = numero + 1;
+        System.out.println(numero); // resultado será o valor 6
+        System.out.println(numero ++); // resultado será 5, porque o interpretador irá imprimir o valor da variavel primeiro
+        System.out.println(numero); // se ele executar o numero++ vai ser 5, se executar essa linha debaixo será 6 porque ele vai fazer o incremento
+        System.out.println(++ numero); //  resultado será 6, porque usei o operador de incremento e depois vai atribuir o incremento na variavel 5
+
+        // A mesma regra é aplicada quando eu quiiser decrementar/diminuir
+
+        System.out.println(numero); // resultado será 5
+        System.out.println(numero --); // resultado será 4
+    }
+    
+    public static void negandoValoresBooleanos (String [] args) {
+
+        boolean variavel = true;
+
+        //caso eu queria negar esse valor de true
+        //System.out.println(!variavel); // resultado será false, este é um valor temporário na memória
+
+        // Reatribuindo o valor da variavel / alterando o valor na memória da variavel
+        variavel = !variavel;
+        System.out.println(variavel); // resultado sera false
+    }
+
+    public static void operadorTernario (String [] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+
+        /* String resultado = ""; // defini o tipo da variavel para que eu possa usar ela no código
+        *  if(a==b)
+        *      resultado = "verdadeiro";
+        *  else
+        *      resultado = "falso";
+        *
+        *  System.out.println(resultado); // resultado será "falso"
+         */
+
+        // Mesmo exemplo, porém com operador ternario
+
+        String resultado = a==b ?"verdadeiro" : "falso"; // resultado será falso
+
+        int resultado2 = a==b ?1:2; // neste exemplo o operador ternario esperado que retorne um tipo inteiro
+
+
+    }
+
+
 }
