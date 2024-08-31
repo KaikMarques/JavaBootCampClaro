c7d03756a07bada04f49642438f52f0d3fc3e11f
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-08-31 09:34:39 -0300

    add usuario.java

--- exercicios/sintaxe/sistema-smart-tv/src/Usuario.java
+++ exercicios/sintaxe/sistema-smart-tv/src/Usuario.java
@@ -6,9 +6,22 @@ public class Usuario {
         // esta é a forma de criar uma nova classe para ilustrar a interação dos métodos e atributos
         SmartTv smartTv = new SmartTv(); 
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual..: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        
+        System.out.println("Canal Atual..: " + smartTv.canal);
+
+        System.out.println("Volume atual : " + smartTv.volume);
+
         // Fazendo a chamada dos atributos/variaveis que criei na minha classe smartTv
         System.out.println("TV Ligada....? " + smartTv.ligada);
-        System.out.println("Canal Atual..: " + smartTv.canal);
+
         System.out.println("Volume Atual : " + smartTv.volume);
 
         smartTv.ligar();
