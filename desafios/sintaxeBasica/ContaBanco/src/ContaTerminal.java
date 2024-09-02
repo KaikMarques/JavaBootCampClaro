fcf83b3252b04e24af49c83547b644c7df27f272
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-02 13:54:39 -0300

    add desafio ContaTerminal na pasta sintaxe-basica em desafio

--- desafios/sintaxeBasica/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxeBasica/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("........... Bem vindo a sua conta! ...........");
+        
+        System.out.println("Digite seu número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número de sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente =  scanner.next();
+
+        System.out.println("Digite o número do seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println();
+        System.out.println(" Obrigado pelas confirmações! ");
+        System.out.println("..............................");
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, ");
+        System.out.println("sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+        
     }
 }
