c29e83d31db6f55dd6a0e84578ac6a5551460254
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-04 12:13:40 -0300

    add controle-fluxo, resultadoEscolar

--- codigos/sintaxe-basica-java/src/bootcamp/claro/controle-fluxo/teste.java
+++ codigos/sintaxe-basica-java/src/bootcamp/claro/controle-fluxo/teste.java
@@ -1,12 +1,22 @@
 public class teste {
   public static void main(String[] args) {
+
+    // Estruturas Condicionais
+
     double saldo = 25.0;
     double valorSolicitado = 17.0;
 
-    if(valorSolicitado < saldo)
-      saldo = saldo - valorSolicitado;
+    /* IF Simples
+     *  if(valorSolicitado < saldo)
+     *  saldo = saldo - valorSolicitado;
+     * System.out.println(saldo);
+     */
 
-    System.out.println(saldo);
+    if(valorSolicitado < saldo) { // No java, se dentro do if ou else eu tenho mais de uma instrução eu preciso criar o bloco {} para coloca outras instruções
+      saldo -= valorSolicitado;
+      System.out.println("Novo Saldo: " + saldo);
+    }else
+      System.out.println("Saldo Insuficiente");
   }
 
 }
