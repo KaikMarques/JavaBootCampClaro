7d2701e6022aa66eeb0f5b7069fa5b139165b794
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-05 09:48:53 -0300

    add class resultadoEscolar

--- codigos/sintaxe-basica-java/src/bootcamp/claro/controle-fluxo/ResultadoEscolar.java
+++ codigos/sintaxe-basica-java/src/bootcamp/claro/controle-fluxo/ResultadoEscolar.java
@@ -7,8 +7,23 @@ public class ResultadoEscolar {
 
     if(nota >= 7)
       System.out.println("Aprovado" );
+    
+    else if (nota >= 5 && nota < 7)
+      System.out.println("Prova de recuperação");
+
     else
-      System.out.println("Reprovado");
+    System.out.println("Reprovado");
+
+    // Usando operador ternario
+
+    // Exemplo 1
+    // String resultado = nota >= 7 ? "aprovado" : "reprovado";
+    // System.out.println(resultado);
+
+    // Exemplo 2
+    // String resultado = nota >= 7 ? "aprovado" : nota >= 5 && nota < 7 ? "recuperação" : "recuperação"; 
+    // System.out.println(resultado);
+
   }
 
 }
