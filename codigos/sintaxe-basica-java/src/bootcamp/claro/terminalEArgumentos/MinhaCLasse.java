dcccec140604157aed9baf2df77ce4854e0a8f4b
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-12 09:36:54 -0300

    add getter and setters

--- codigos/sintaxe-basica-java/src/bootcamp/claro/terminalEArgumentos/MinhaCLasse.java
+++
@@ -1,7 +0,0 @@
-package bootcamp.claro.terminalEArgumentos;
-
-public class MinhaCLasse {
-    public static void main(String[] args) {
-        System.out.println("Fui executado pelo terminal");
-    }
-}
