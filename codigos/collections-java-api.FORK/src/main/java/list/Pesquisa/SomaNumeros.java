7cde944916f978a4c138dff014cfcca91e88baae
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-19 18:21:05 -0300

    add método calcularSoma()

--- codigos/collections-java-api.FORK/src/main/java/list/Pesquisa/SomaNumeros.java
+++ codigos/collections-java-api.FORK/src/main/java/list/Pesquisa/SomaNumeros.java
@@ -1,9 +1,26 @@
 package main.java.list.Pesquisa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SomaNumeros {
 
+    private List<Integer> numeros;
 
+    public SomaNumeros() {
+        this.numeros = new ArrayList<>();
+    }
 
+    public void adicionarNumero(int numero) {
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for( Integer numero : numeros )
+            soma += numero;
+        return soma;
+    }
 
 
 
