adff054e955c4a8014ca1cd48efb805bf059e9c9
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-09-19 16:53:18 -0300

    add carinho de compra

--- codigos/collections-java-api.FORK/src/main/java/list/OperacoesBasicas/CarrinhoDeCompra.java
+++ codigos/collections-java-api.FORK/src/main/java/list/OperacoesBasicas/CarrinhoDeCompra.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoDeCompra {
+
     private List<Item> itemList;
 
     public CarrinhoDeCompra( ) {
@@ -15,21 +16,34 @@ public class CarrinhoDeCompra {
         Item item = new Item(nome, preco, quantidade);
         this.itemList.add(item);
     }
+
+
     // Tarefa 2 - removerItem(String nome)
+    // Método para remover um item do carrinho com base no nome do item
     public void removerItem(String nome) {
-        // Criando uma lista temporária
+
+        // Cria uma lista temporária para armazenar os itens que serão removidos
         List<Item> itensParaRemover = new ArrayList<>();
-        if(!itemList.isEmpty()) {
+        // Verifica se a lista de itens (itemList) não está vazia
+        if (!itemList.isEmpty()) {
+            // Percorre a lista de itens do carrinho (itemList)
             for (Item i : itemList) {
+                // Verifica se o nome do item atual é igual ao nome passado como argumento
+                // O método equalsIgnoreCase() compara os nomes ignorando diferenças de maiúsculas e minúsculas
                 if (i.getNome().equalsIgnoreCase(nome)) {
+
+                    // Se o nome coincidir, o item é adicionado à lista temporária de itens a serem removidos
                     itensParaRemover.add(i);
                 }
             }
+            // Remove todos os itens da lista original (itemList) que estão presentes na lista temporária (itensParaRemover)
             itemList.removeAll(itensParaRemover);
         } else {
+            // Se a lista de itens estiver vazia, imprime uma mensagem informando que não há itens para remover
             System.out.println("A lista está vazia!");
         }
     }
+
 
 
 
